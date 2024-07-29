feb2cc569361fbcb6c05be1f930ec0456283cf9a
ALPHA ALVIN <alpha.muchemi@student.moringaschool.com>
2024-07-29 13:41:05 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+function countDown(number) {
+    let currentNumber = number;
+    while (currentNumber >= 0) {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
+
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
